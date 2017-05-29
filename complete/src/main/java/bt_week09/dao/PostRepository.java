d17452813c126d9b70713e3cbfa4a3eb5198e9d9
MinhThuong96 <minhthuong12a15@gmail.com>
2017-05-29 11:21:39 +0700

    deploy

---
+++ complete/src/main/java/bt_week09/dao/PostRepository.java
@@ -0,0 +1,9 @@
+package bt_week09.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import bt_week09.model.Post;
+
+public interface PostRepository extends CrudRepository<Post, Integer>  {
+
+}
---
+++ initial/src/main/java/bt_week09/storge/StorageException.java
@@ -0,0 +1,13 @@
+package bt_week09.storge;
+
+@SuppressWarnings("serial")
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
